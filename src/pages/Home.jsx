5e4db52882d5766e123213d86adaeafd9f7ef720
lilgibs <lil.gibran01@gmail.com>
2023-08-04 15:59:07 +0700

    Feat: Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import { BiLogOut } from 'react-icons/bi';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { checkLoginUser, resetUser } from '../features/userSlice'
 import { useNavigate } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 
 function Home() {
@@ -49,80 +50,84 @@ function Home() {
 
 
   return (
-
-    <div className='w-[85%] lg:max-w-6xl mx-auto flex flex-col gap-3 min-h-screen'>
-      <div className=' border bg-white text-teal-500 shadow-md text-center font-semibold text-2xl sm:text-3xl md:text-5xl py-6 md:py-10 rounded mt-2'>
-        <h1>ITEM LIST</h1>
-      </div>
-      <div className='flex flex-row gap-5 items-center justify-between bg-white border rounded p-2 md:p-4'>
-        <p className='md:text-lg font-semibold text-neutral-500'>Hello, <span className=' text-teal-500'>{userGlobal.name}</span></p>
-        <button
-          className='flex gap-2 items-center px-2 md:px-4 py-2 md:py-2 text-sm md:text-md bg-teal-500 text-white rounded font-semibold hover:bg-teal-700'
-          onClick={() => {
-            dispatch(resetUser())
-          }}
-        >
-          <BiLogOut />
-          <p>Logout</p>
-        </button>
-      </div>
-      <div className='flex flex-row-reverse gap-5 justify-between bg-white border rounded p-2 md:p-4'>
-        <div className='flex-grow'>
-          <form onSubmit={handleSearchSubmit}>
-            <div className="flex justify-between w-full text-sm md:text-md">
-              <input
-                className='border-l border-b border-t rounded-s-md px-4 focus:border-teal-500 focus:outline-none w-full'
-                value={filterValue}
-                onChange={(e) => setFilterValue(e.target.value)}
-                placeholder="Search by item name ..."
-              />
-              <button
-                className="bg-teal-500 hover:bg-teal-600 font-semibold text-white py-2 md:py-3 px-4 rounded-e-md"
-                type='submit'
-              >
-                <FaSearch />
-              </button>
-            </div>
-          </form>
+    <>
+      <div className='w-[85%] lg:max-w-6xl mx-auto flex flex-col gap-3 min-h-screen'>
+        <div className=' border bg-white text-teal-500 shadow-md text-center font-semibold text-2xl sm:text-3xl md:text-5xl py-6 md:py-10 rounded mt-2'>
+          <h1>ITEM LIST</h1>
         </div>
-        <div>
+        <div className='flex flex-row gap-5 items-center justify-between bg-white border rounded p-2 md:p-4'>
+          <p className='md:text-lg font-semibold text-neutral-500'>Hello, <span className=' text-teal-500'>{userGlobal.name}</span></p>
           <button
-            className='flex gap-2 items-center px-2 md:px-4 h-full md:py-2 text-sm md:text-md bg-teal-500 text-white rounded font-semibold hover:bg-teal-700'
-            onClick={onAddOpen}
+            className='flex gap-2 items-center px-2 md:px-4 py-2 md:py-2 text-sm md:text-md bg-teal-500 text-white rounded font-semibold hover:bg-teal-700'
+            onClick={() => {
+              dispatch(resetUser())
+            }}
           >
-            <FaPlus />
-            <p className='hidden md:block'>Add Item</p>
+            <BiLogOut />
+            <p>Logout</p>
           </button>
         </div>
-        <div className='hidden'>
-          <AddItemModal isOpen={isAddOpen} onClose={onAddClose} />
+        <div className='flex flex-row-reverse gap-5 justify-between bg-white border rounded p-2 md:p-4'>
+          <div className='flex-grow'>
+            <form onSubmit={handleSearchSubmit}>
+              <div className="flex justify-between w-full text-sm md:text-md">
+                <input
+                  className='border-l border-b border-t rounded-s-md px-4 focus:border-teal-500 focus:outline-none w-full'
+                  value={filterValue}
+                  onChange={(e) => setFilterValue(e.target.value)}
+                  placeholder="Search by item name ..."
+                />
+                <button
+                  className="bg-teal-500 hover:bg-teal-600 font-semibold text-white py-2 md:py-3 px-4 rounded-e-md"
+                  type='submit'
+                >
+                  <FaSearch />
+                </button>
+              </div>
+            </form>
+          </div>
+          <div>
+            <button
+              className='flex gap-2 items-center px-2 md:px-4 h-full md:py-2 text-sm md:text-md bg-teal-500 text-white rounded font-semibold hover:bg-teal-700'
+              onClick={onAddOpen}
+            >
+              <FaPlus />
+              <p className='hidden md:block'>Add Item</p>
+            </button>
+          </div>
+          <div className='hidden'>
+            <AddItemModal isOpen={isAddOpen} onClose={onAddClose} />
+          </div>
         </div>
-      </div>
-      <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 bg-white rounded border p-4'>
-        {/* {(filteredItems || itemDatas).map(item => (
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 bg-white rounded border p-4'>
+          {/* {(filteredItems || itemDatas).map(item => (
           <ItemCard item={item} />
         ))} */}
-        {currentPageData.length ? currentPageData : <p className=''>Looks like you have no items. Start by adding one!</p>}
+          {currentPageData.length ? currentPageData : <p className=''>Looks like you have no items. Start by adding one!</p>}
+        </div>
+        <ReactPaginate
+          previousLabel={<IoIosArrowBack />}
+          nextLabel={<IoIosArrowForward />}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination flex justify-center gap-4 items-center text-lg"}
+          pageLinkClassName={"px-1"}
+          previousLinkClassName={"previous-link"}
+          previousClassName="pt-1"
+          nextClassName="pt-1"
+          nextLinkClassName={"next-link"}
+          disabledClassName={"disabled text-neutral-400"}
+          activeClassName={`active font-semibold text-teal-500 border-b-2 border-b-teal-500 `}
+        />
       </div>
-      <ReactPaginate
-        previousLabel={<IoIosArrowBack />}
-        nextLabel={<IoIosArrowForward />}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination flex justify-center gap-4 items-center text-lg"}
-        pageLinkClassName={"px-1"}
-        previousLinkClassName={"previous-link"}
-        previousClassName="pt-1"
-        nextClassName="pt-1"
-        nextLinkClassName={"next-link"}
-        disabledClassName={"disabled text-neutral-400"}
-        activeClassName={`active font-semibold text-teal-500 border-b-2 border-b-teal-500 `}
-      />
-    </div>
+      <Footer />
+    </>
+
+
   )
 }
 
